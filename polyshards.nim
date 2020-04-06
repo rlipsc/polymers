160f698cfe084e126504e74f31846a0d34cf62e3
rlipsc <36367371+rlipsc@users.noreply.github.com>
2020-04-06 23:22:09 +0100

    Add ecs_opengl export, update misc names

--- polyshards.nim
+++ polyshards.nim
@@ -1,11 +1,13 @@
 import console/[ecs_renderchar, ecs_consoleevents, ecs_mousebuttons]
 import network/[ecs_net]
 import db/[ecs_db, ecs_db_threads]
-import misc/[ecskilling, ecsspawnafter]
+import misc/[ecs_killing, ecs_spawnafter]
+import graphics/[ecs_opengl]
 
 export
   ecs_renderchar, ecs_consoleevents, ecs_mousebuttons,
   ecs_net,
   ecs_db, ecs_db_threads,
-  ecs_killing, ecs_spawnafter
+  ecs_killing, ecs_spawnafter,
+  ecs_opengl
 
