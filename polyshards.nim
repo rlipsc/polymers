9e2528e74b010391211a31a8d3ca98d45bb726ed
rlipsc <36367371+rlipsc@users.noreply.github.com>
2020-04-07 02:31:11 +0100

    Update ecs_net to ecs_udp

--- polyshards.nim
+++ polyshards.nim
@@ -1,12 +1,12 @@
 import console/[ecs_renderchar, ecs_consoleevents, ecs_mousebuttons]
-import network/[ecs_net]
+import network/[ecs_udp]
 import db/[ecs_db, ecs_db_threads]
 import misc/[ecs_killing, ecs_spawnafter]
 import graphics/[ecs_opengl]
 
 export
   ecs_renderchar, ecs_consoleevents, ecs_mousebuttons,
-  ecs_net,
+  ecs_udp,
   ecs_db, ecs_db_threads,
   ecs_killing, ecs_spawnafter,
   ecs_opengl
