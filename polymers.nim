21eb3f0e6dc4664b9bbbd0163626c7a7311f38aa
rlipsc <36367371+rlipsc@users.noreply.github.com>
2020-11-09 14:47:04 +0000

    Export grid map and chipmunk physics components

--- polymers.nim
+++ polymers.nim
@@ -1,13 +1,15 @@
 import console/[ecs_renderchar, ecs_consoleevents, ecs_mousebuttons]
 import network/[ecs_udp]
 import db/[ecs_db, ecs_db_threads]
-import misc/[ecs_killing, ecs_spawnafter]
+import misc/[ecs_killing, ecs_spawnafter, ecs_gridmap]
+import physics/[ecs_chipmunk2d]
 import graphics/[ecs_opengl]
 
 export
   ecs_renderchar, ecs_consoleevents, ecs_mousebuttons,
   ecs_udp,
   ecs_db, ecs_db_threads,
-  ecs_killing, ecs_spawnafter,
+  ecs_killing, ecs_spawnafter, ecs_gridmap,
+  ecs_chipmunk2d,
   ecs_opengl
 
