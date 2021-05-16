d7e8fa734a78cfde987781934773a7b5c7dbd314
rlipsc <36367371+rlipsc@users.noreply.github.com>
2021-05-16 16:31:02 +0100

    Allow supplying parameters from the command line
    - Includes some comments with example values at the end of the program.

--- demos/particlelife.nim
+++ demos/particlelife.nim
@@ -5,22 +5,78 @@ from math import TAU, PI, degToRad, radToDeg, cos, sin, arctan2, sqrt
   Inspired by the particle system described here:
     https://www.youtube.com/watch?v=makaJpLvbow
     More info: https://www.nature.com/articles/srep37969
+  
   Press space to set a random turn factor for all particles,
   press R to randomise positions.
+  
   Best run with -d:danger.
 ]#
 
+import strutils, os
+
 randomize()
 
-const maxEnts = 30000
+type
+  Commands = enum
+    cmdTurn = "turn",
+    cmdRadius = "radius",
+    cmdTVariance = "randturn",
+    cmdRVariance = "randradius",
+
+var params: array[Commands, tuple[given: bool, value: float]]
+let numParams = paramCount()
+
+if numParams >= 1:
+  for p in 1..min(numParams, params.len):
+    let
+      param = paramStr(p).toLowerAscii
+      eq = param.find('=')
+    if eq > 0:
+      let
+        command = param[0 ..< eq]
+        right = param[eq + 1 .. ^1]
+      if right.len == 0:
+        quit "Commands must include a value: \"" & param & "\""
+      case command
+      of $cmdTurn:
+        params[cmdTurn] = (true, parseFloat(right))
+      of $cmdRadius:
+        params[cmdRadius] = (true, parseFloat(right))
+      of $cmdTVariance:
+        params[cmdTVariance] = (true, parseFloat(right))
+      of $cmdRVariance:
+        params[cmdRVariance] = (true, parseFloat(right))
+      else:
+        quit "Unknown command: \"" & command & "\" in parameter \"" & param & "\""
+
+# Commands: <turn around>, <cluster radius>, <cluster variance>
+let
+  turnAmount =
+    if params[cmdTurn].given: params[cmdTurn].value
+    else: rand -PI..PI
+  clusterSize =
+    if params[cmdRadius].given: params[cmdRadius].value
+    else: 0.03
+  turnVariance =
+    if params[cmdTVariance].given: params[cmdTVariance].value
+    else: 0.0
+  clusterVariance = 
+    if params[cmdRVariance].given: params[cmdRVariance].value
+    else: 0.00
+
+echo "TurnAround: ", turnAmount, " (", turnAmount.radToDeg, "), Turn variance: ", turnVariance
+echo "Cluster size: ", clusterSize, ", Cluster variance: ", clusterVariance
+
+const maxEnts = 30000 #45000
 const
   compOpts = fixedSizeComponents(maxEnts)
   sysOpts = fixedSizeSystem(maxEnts)
   entOpts = fixedSizeEntities(maxEnts)
-  dt = 1.0 / 60.0
+
+echo "Particles: ", maxEnts
 
 defineOpenGlRenders(compOpts, sysOpts)
-defineGridMap(0.02, Position, compOpts, sysOpts)
+defineGridMap(0.03, Position, compOpts, sysOpts)
 
 registerComponents(compOpts):
   type
@@ -95,7 +151,7 @@ makeSystemOptFields("setTurnAmount", [Cluster], sysOpts) do:
 do:
   # Changes Cluster then deactivates.
   init: sys.paused = true
-  all: item.cluster.turnAmount = sys.turnAmount
+  all: item.cluster.turnAmount = sys.turnAmount + rand -turnVariance .. turnVariance
   finish: sys.paused = true
 
 makeSystemOpts("randomisePositions", [Position], sysOpts):
@@ -144,18 +200,6 @@ let
 
 squareModel.setMaxInstanceCount(maxSquares)
 
-import strutils, os
-
-const showFps = defined(showFps)
-let turnAmount =
-  if paramCount() > 0:
-    # Pass turnAround in command line.
-    parseFloat(paramStr(0))
-  else:
-    rand PI
-echo "TurnAround: ", turnAmount, " (", turnAmount.radToDeg, ")"
-echo "Particles: ", maxEnts
-
 # Create some entities with our models.
 for i in 0 ..< maxEnts:
   let
@@ -168,24 +212,53 @@ for i in 0 ..< maxEnts:
     Model(modelId: squareModel, scale: vec3(scale), angle: rand(TAU), col: vec4(rand colRange, rand colRange, rand colRange, 1.0)),
     Position(x: rand(-1.0..1.0), y: rand(-1.0..1.0), z: 0.0),
     Velocity(speed: speed),
-    #Direction(),
-    Cluster(radius: 0.03, turnAmount: turnAmount),
+    Cluster(
+      radius: max(0.001, clusterSize + rand(-clusterVariance .. clusterVariance)),
+      turnAmount: turnAmount + rand(-turnVariance .. turnVariance)),
     Rotate(angle: rand TAU, rotateSpeed: PI),
     GridMap()
   )
 
 import times, strformat
 
-when showFps:
-  var
-    t1 = cpuTime()
-    fc: int
-
 type KeyCodes = ptr array[0 .. SDL_NUM_SCANCODES.int, uint8]
 proc pressed(keyStates: KeyCodes, sc: Scancode): bool = result = keyStates[sc.int] > 0'u8
 
+import algorithm
+
 proc main =
   echo "Keys: R = Randomise positions, Space = Randomise turn amount."
+
+  proc systemPerformance(budget: float, count: Natural = 0): string =
+    ## Return a string with the top on any system performance
+    var sysTime: float
+    forAllSystems:
+      when compiles(sys.timePerRun):
+        sysTime += sys.timePerRun
+
+    type Perf = tuple[secs: float, name: string]
+    var perfs = newSeqOfCap[Perf](totalSystemCount)
+
+    forAllSystems:
+      when compiles(sys.timePerRun):
+        if sys.timePerRun > 0.0:
+          perfs.add (sys.timePerRun, sys.name)
+    
+    perfs.sort do (x, y: Perf) -> int:
+      result = cmp(y.secs, x.secs)
+
+    result = "Performance:\n"
+    let c = if count > 0: min(count, perfs.len) else: perfs.len
+    for i in 0 ..< c:
+      let perf = (perfs[i].secs / budget) * 100.0
+      result &= &"Budget: {perf:>4.2f}% {perfs[i].name}, time {perfs[i].secs:>4.4}\n" 
+
+  const showFps = defined(showFps)
+  when showFps:
+    var
+      t1 = cpuTime()
+      fc: int
+
   # Render loop.
   while running:
     while pollEvent(evt):
@@ -199,7 +272,7 @@ proc main =
         let ta = rand -25.0.degToRad..25.0.degToRad
         sysSetTurnAmount.turnAmount = ta
         sysSetTurnAmount.paused = false
-        echo &"Set turn amount to {ta} ({ta.radToDeg:>4.4f} degrees)"
+        echo &"Set turn amount to {ta} ({ta.radToDeg:>4.4f} degrees), variance {turnVariance}"
 
       if keyStates.pressed(SDL_SCANCODE_R):
         # Randomise positions of all particles.
@@ -210,6 +283,7 @@ proc main =
     run()
     renderModels()
 
+
     when showFps:
       fc += 1
       let t2 = cpuTime()
@@ -217,7 +291,13 @@ proc main =
         echo "FPS: ", fc
         t1 = cpuTime()
         fc = 0
+        echo systemPerformance(1/60, 3)
 
     window.glSwapWindow()
-
+# Cellular division: turn amount: -0.1543731093818758 (-8.8449 degrees) (or positive)
+# Radial branching: TurnAround: 0.2541798816729668 (14.56343445699566)
+# Undulating phonons: TurnAround: 0.7617203492334945 (43.64336118031036)
+# Liquid in the middle: TurnAround: 0.2350809840462089 (13.46914822963001) Cluster size: 0.03 Cluster variance: 0.01
+# Static hexagonal: Turn amount to 0.202731770326035 (11.6157 degrees) Cluster size: 0.03 Cluster variance: 0.01
+# Reorganising city: Set turn amount to -0.2239368030723899 (-12.8306 degrees) Cluster size: 0.03 Cluster variance: 0.01
 main()
