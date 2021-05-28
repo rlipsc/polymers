5220324fc87647e740fd74c408b77553a0e5178e
rlipsc <36367371+rlipsc@users.noreply.github.com>
2021-05-28 23:23:17 +0100

    Forward ecs_http and ecs_jsonrpc

--- polymers.nim
+++ polymers.nim
@@ -1,5 +1,5 @@
 import console/[ecs_renderchar, ecs_consoleevents, ecs_mousebuttons]
-import network/[ecs_udp, ecs_tcp]
+import network/[ecs_udp, ecs_tcp, ecs_http, ecs_jsonrpc]
 import db/[ecs_db, ecs_db_threads]
 import misc/[ecs_killing, ecs_spawnafter]
 import spatial/ecs_gridmap
@@ -8,7 +8,7 @@ import graphics/[ecs_opengl]
 
 export
   ecs_renderchar, ecs_consoleevents, ecs_mousebuttons,
-  ecs_udp, ecs_tcp
+  ecs_udp, ecs_tcp, ecs_http, ecs_jsonrpc,
   ecs_db, ecs_db_threads,
   ecs_killing, ecs_spawnafter, ecs_gridmap,
   ecs_chipmunk2d,
