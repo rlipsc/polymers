b44d635d1a08c55d92a73d13e8a870cba1185be9
rlipsc <36367371+rlipsc@users.noreply.github.com>
2021-05-29 22:44:25 +0100

    Add simplewebsite demo

---
+++ demos/simplewebsite.nim
@@ -0,0 +1,60 @@
+import polymorph, polymers, times
+
+const
+  maxEnts   = 10_000  # Effectively the maximum concurrent sockets.
+  entOpts   = fixedSizeEntities(maxEnts)
+  compOpts  = fixedSizeComponents(maxEnts)
+  sysOpts   = fixedSizeSystem(maxEnts)
+
+# Define networking components.
+defineTCPNetworking(compOpts, sysOpts, tllEvents)
+defineHttp(compOpts, sysOpts)
+
+# Web page components.
+registerComponents(compOpts):
+  type
+    RootPage* = object
+    TimePage* = object
+
+makeSystemOptFields("serveRoot", [RootPage], sysOpts):
+  helloCount: int
+do:
+  added:
+    item.entity.add HttpResponse(
+      code: Http200,
+      body: "Hello " & $sys.helloCount
+    )
+    sys.helloCount += 1
+
+makeSystemOpts("serveTime", [TimePage], sysOpts):
+  added:
+    item.entity.add HttpResponse(
+      code: Http200,
+      body: "The time is " & getClockStr()
+    )
+
+makeSystemOpts("finishServePage", [HttpResponseSent], sysOpts):
+  # Clean up after a response.
+  added: sys.deleteList.add item.entity
+
+makeEcs(entOpts)
+commitSystems("poll")
+
+let
+  server =
+    newEntityWith(
+      TCPListen(
+        port: Port(5555),
+        onAccept: cl(
+          ProcessHttp(),
+          HttpRouteEntity(
+            patterns: @[
+              ("/", cl RootPage()),
+              ("/time", cl TimePage())
+            ]
+          )
+        )
+      )
+    )
+
+while true: poll()
