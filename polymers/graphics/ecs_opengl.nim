cddd1c8aba5bbe72d1f9948b4b2507277e5e0557
rlipsc <36367371+rlipsc@users.noreply.github.com>
2022-02-05 03:33:41 +0000

    Add aspect ratio support to ecs_opengl

--- polymers/graphics/ecs_opengl.nim
+++ polymers/graphics/ecs_opengl.nim
@@ -33,26 +33,37 @@ template defineOpenGlComponents*(compOpts: ECSCompOptions, positionType: typedes
 
 macro defineOpenGLUpdateSystems*(sysOpts: ECSSysOptions, positionType: typedesc): untyped =
   ## Add the system to send model and position data to the GPU.
-  
-  let posIdent = ident toLowerAscii($positionType)
+  # Note: these are embedded in a macro so we can insert the user's 'positionType'.
+  let
+    posIdent = ident toLowerAscii($positionType)
 
   result = newStmtList(quote do:
     makeSystemOpts("updateModelData", [Model, `positionType`], `sysOpts`):
       # This system populates the instance buffers and keeps track of
       # how many instances need to be rendered per model.
+      
       fields:
         curModelCount {.pub.}: seq[int]
+        aspectRatio {.pub.} = 1.0
+        lastAspect = 0.0
 
-      # Reset model counters.
+      # Init model info.
       sys.curModelCount.setLen modelCount()
+
       for i in 0 ..< sys.curModelCount.len:
         sys.curModelCount[i] = 0
+
+      if sys.lastAspect != sys.aspectRatio:
+        sys.lastAspect = sys.aspectRatio
+        for i in 0 ..< sys.curModelCount.len:
+          i.ModelId.aspect = sys.aspectRatio
 
       all:
         # Populate instance buffers.
         let
           mId = item.model.modelId
           curPos = sys.curModelCount[mId.int]
+        
         mId.positionVBOArray[curPos] = vec3(item.`posIdent`.x, item.`posIdent`.y, item.`posIdent`.z)
         mId.scaleVBOArray[curPos] = item.model.scale
         mId.rotationVBOArray[curPos] = [item.model.access.angle]
@@ -60,24 +71,36 @@ macro defineOpenGLUpdateSystems*(sysOpts: ECSSysOptions, positionType: typedesc)
 
         sys.curModelCount[mId.int].inc
 
+
     makeSystemOpts("updateTextureData", [Texture, `positionType`], `sysOpts`):
       # This system populates billboard instances.
+      
       fields:
         billboards {.pub.}: seq[TexBillboard]
+        aspectRatio {.pub.} = 1.0
+        lastAspect = 0.0
 
-      # Reset texture counters.
+      # Init texture info.
       for i in 0 ..< sys.billboards.len:
         sys.billboards[i].resetItemPos
+        
+      if sys.lastAspect != sys.aspectRatio:
+        sys.lastAspect = sys.aspectRatio
+        
+        for i in 0 ..< sys.billboards.len:
+          sys.billboards[i].updateAspect sys.aspectRatio
 
       all:
         # Populate instances.
         let
           tId = item.texture.textureId
+        
         sys.billboards[tId.int].addItems(1):
           curItem.positionData =  vec4(item.`posIdent`.x, item.`posIdent`.y, item.`posIdent`.z, 1.0)
           curItem.colour =        item.texture.col
           curItem.rotation[0] =   item.texture.angle
           curItem.scale =         item.texture.scale
+
 
     iterator activeModels*: tuple[model: ModelId, count: int] =
       ## Iterate models that have a non-zero count and been processed by
@@ -98,22 +121,30 @@ macro defineOpenGLUpdateSystems*(sysOpts: ECSSysOptions, positionType: typedesc)
       ## Code in `setup` will be run before each model is rendered.
       for modelInfo in activeModels():
         renderModelSetup(modelInfo.model, modelInfo.count, setup)
-    
+
+    proc setModelAspectRatios*(ratio: float) =
+      assert ratio != 0, "Aspect ratio must be non-zero"
+      sysUpdateModelData.aspectRatio = ratio
+
     proc renderActiveTextures* = 
       for i in 0 ..< sysUpdateTextureData.billboards.len:
-        template bb: untyped = sysUpdateTextureData.billboards[i]
-        bb.render
+        sysUpdateTextureData.billboards[i].render
 
     proc newTextureId*(vertexGLSL = defaultTextureVertexGLSL, fragmentGLSL = defaultTextureFragmentGLSL,
         max = 1, model: openarray[TextureVertex] = rectangle, modelScale = 1.0, manualTextureBo = false, manualProgram = false): TextureId =
+      
       sysUpdateTextureData.billboards.add newTexBillboard(vertexGLSL = vertexGLSL, fragmentGLSL = fragmentGLSL, max = max, model = model,
         modelScale = modelScale, manualTextureBo = manualTextureBo, manualProgram = manualProgram)
       result = sysUpdateTextureData.billboards.high.TextureId
-      template bb: untyped = sysUpdateTextureData.billboards[result.int]
-      bb.rotMat[0] = 1.0
+      sysUpdateTextureData.billboards[result.int].rotMat[0] = 1.0
     
     proc update*(textureId: TextureId, texture: GLTexture) =
       sysUpdateTextureData.billboards[textureId.int].updateTexture(texture)
+    
+    proc setTextureAspectRatios*(ratio: float) =
+      assert ratio != 0, "Aspect ratio must be non-zero"
+      sysUpdateTextureData.aspectRatio = ratio
+
   )
 
 template defineOpenGlRenders*(compOpts: ECSCompOptions, sysOpts: ECSSysOptions, positionType: typedesc) {.dirty.} =
@@ -125,6 +156,7 @@ template defineOpenGlRenders*(compOpts: ECSCompOptions, sysOpts: ECSSysOptions) 
     type
       Position* = object
         x*, y*, z*: float
+  
   defineOpenGLComponents(compOpts, Position)
   defineOpenGLUpdateSystems(sysOpts, Position)
 
