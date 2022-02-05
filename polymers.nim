7ca47a2b6a20053c5ea07a8ff696563c0a5d5cef
rlipsc <36367371+rlipsc@users.noreply.github.com>
2022-02-05 14:49:18 +0000

    Export ecs_fonttext, update nimble test locations

--- polymers.nim
+++ polymers.nim
@@ -4,7 +4,7 @@ import polymers/db/[ecs_db, ecs_db_threads]
 import polymers/misc/[ecs_killing, ecs_spawnafter]
 import polymers/spatial/ecs_gridmap
 import polymers/physics/ecs_chipmunk2D
-import polymers/graphics/[ecs_opengl]
+import polymers/graphics/[ecs_opengl, ecs_fonttext]
 
 export
   ecs_renderchar, ecs_consoleevents, ecs_mousebuttons,
@@ -12,5 +12,5 @@ export
   ecs_db, ecs_db_threads,
   ecs_killing, ecs_spawnafter, ecs_gridmap,
   ecs_chipmunk2d,
-  ecs_opengl
+  ecs_opengl, ecs_fonttext
 
